766a097bcfd30b32c3af955189a7d62d613518c1
Brayan Soares <brayanmesquita@gmail.com>
2022-09-07 16:18:32 -0400

    brayan-Inspiron-7472, 2022-09-07

--- curso_data_science/ListaEncadeada.py
+++
@@ -1,7 +0,0 @@
-class ItemLista:
-    def __init__(self, data=0, nextItem=None):
-        self.data = data
-        self.nextItem = None
-    def __repr__(self):
-        return '%s' % (self.data, self.nextItem)
-
