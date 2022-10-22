ecf49f5cd25191ab27148d2cc41d1bc634353e30
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-10-21 22:00:49 -0400

    teste

--- pequenos_programas/_jogorapido.py
+++ pequenos_programas/_jogorapido.py
@@ -1,14 +1,2 @@
-# palavra = list('brayan')
-# ocultar = ['-' for x in palavra]
-# s = '-'
-# print(f'{s.join(palavra)}')
-import random
-def carrega_palavra_secreta():
-    arquivos_de_letras = open('palavras.txt', 'r')
-    lista_palavras = []
-    for palavra in arquivos_de_letras:
-        lista_palavras.append(palavra.strip())
-    indice_secreto = random.randint(0, (len(lista_palavras)+1))
-    return len(lista_palavras)+1
-    #return lista_palavras[indice_secreto]
-print(carrega_palavra_secreta())
+option = '12'
+print(option[0])
