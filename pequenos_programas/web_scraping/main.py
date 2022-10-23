c8a13412cc5be51e4956d291454f0d72815313d5
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-10-22 21:48:47 -0400

    MacBook-Air-de-brayan.local em 22/10/2022 21:48

--- pequenos_programas/web_scraping/main.py
+++ pequenos_programas/web_scraping/main.py
@@ -8,6 +8,22 @@ response = requests.get(url)
 #bs4
 page = BeautifulSoup(response.text, 'html.parser')
 
-a = page.title
-texto = a.text.split(' ')[1].upper()
-texto
+page.title.string#nome da pagina
+
+#======= procurar elementos======
+h2, h3 = page.find_all('h2'), page.find_all('h3')
+type(h3)#bs4.element.ResultSet
+
+#extrair texto de todos h3
+section = []
+h3_chars = page.find('h3', attrs={'id': 'chars'})
+for element in h3_chars.next_elements:
+    if element.name == 'h3':
+        break
+    section.append(element.string or '')
+result = ''.join(section)
+result#resultado de texto sem html, uma verdadeira bagunca he
+
+#======= adicionando elementos======
+h3[-1].append(' Adicionando mais conteudo')
+h3[-1]
