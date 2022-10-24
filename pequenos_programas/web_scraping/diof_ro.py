080e0d2804fb9ed28c2686161bc5e28fe872a2e5
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-10-24 16:33:23 -0400

    windows_ini_w

--- pequenos_programas/web_scraping/diof_ro.py
+++ pequenos_programas/web_scraping/diof_ro.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 #requests
 url = 'https://diof.ro.gov.br/'
@@ -8,12 +9,25 @@ response = requests.get(url)
 page = BeautifulSoup(response.text, 'html.parser')
 title_page = page.title
 
-links = []
-for link in page.find_all('a'):
-    links.append(link.get('href'))
+def search_all_links():
+    links_a = []
+    for link in page.find_all('a'):
+        links_a.append(link.get('href'))
+    return links_a
 
-for link in links:
-    print(link)
+def search_doe(lista, word):
+    links_doe = []
+    for x in lista:
+        if word in x:
+            links_doe.append(x)
+    return links_doe
+
+links_doe = search_doe(search_all_links(), 'pdf')
+for doe in links_doe:
+    print(doe[-14:-4])#DOE-24.10.2022
+
 #fazer donwload
-# file = 'arquivolocal.txt'
-# request.urlretrieve(file_url , file )
+#data_e_hora_atuais = datetime.now()
+#data_e_hora_atuais = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+links_com_pdf = [valor for valor in links_doe if 'pdf'  in valor ]
+
