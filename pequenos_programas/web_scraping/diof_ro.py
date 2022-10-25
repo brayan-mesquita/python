c6590973c41ae96740a50e7042b3bb0e518cae7d
Brayan Mesquita <60306164+brayan-mesquita@users.noreply.github.com>
2022-10-25 15:52:14 -0400

    sbind

--- pequenos_programas/web_scraping/diof_ro.py
+++ pequenos_programas/web_scraping/diof_ro.py
@@ -27,6 +27,8 @@ for doe in links_doe:
     print(doe[-14:-4])#DOE-24.10.2022
 
 #fazer donwload
+
+
 #data_e_hora_atuais = datetime.now()
 #data_e_hora_atuais = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
 links_com_pdf = [valor for valor in links_doe if 'pdf'  in valor ]
